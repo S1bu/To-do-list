df7f017a3839df14a7b3b37a24cad7af1ece01f3
Sibusiso <Sibusisodyan@gmail.com>
2023-06-02 20:02:22 +0200

    list is listing

--- script/code.js
+++ script/code.js
@@ -1,16 +1,20 @@
  function getTask(){
+    // the store room array
 let list =[];
 
     // gets the value from input class
 let task = document.querySelector('.input').value;
 
+console.log(task)
 // store my list in an array
-list.push(task);
+list.push(task)
+ 
 
 console.log(list)
 // display the value in output
- document.querySelector('.output-list').innerHTML=
+ document.querySelector('.output-list').innerHTML +=
  `
  <li>${task}</li>
  `
  }
+ 
